fff7b451ac7fbe377ec863ae921c849a430ec9c2
Nikita Chernov <drimchansky@gmail.com>
2020-08-02 13:50:38 +0300

    Adjust SSR

--- components/Layout.js
+++ components/Layout.js
@@ -1,22 +1,12 @@
 // packages
 import styled from 'styled-components'
-// components
-import Head from 'next/head'
-// util
-import GlobalStyles from '../utils/GlobalStyles'
 
 const StyledLayout = styled.div``
 
 const Layout = ({ children }) => {
   return (
     <>
-      <Head>
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
-      </Head>
-      <StyledLayout>
-        <GlobalStyles />
-        {children}
-      </StyledLayout>
+      <StyledLayout>{children}</StyledLayout>
     </>
   )
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,33 @@
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+import React from 'react'
+import App from 'next/app'
+import Head from 'next/head'
+import { ThemeProvider } from '@material-ui/styles'
+import CssBaseline from '@material-ui/core/CssBaseline'
+import GlobalStyles from '../utils/GlobalStyles'
+
+class MyApp extends App {
+  componentDidMount() {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector('#jss-server-side')
+    if (jssStyles) {
+      jssStyles.parentNode.removeChild(jssStyles)
+    }
+  }
+
+  render() {
+    const { Component, pageProps } = this.props
+
+    return (
+      <>
+        <Head>
+          <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width" />
+        </Head>
+        <GlobalStyles />
+        <CssBaseline />
+        <Component {...pageProps} />
+      </>
+    )
+  }
 }
 
-export default MyApp;
+export default MyApp
