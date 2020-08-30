6ed515b3afdc78c319811f8a5aa0c7579fead765
Nikita Chernov <drimchansky@gmail.com>
2020-08-30 15:29:51 +0300

    tests: Initialize tests

---
+++ .jest.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  setupFilesAfterEnv: ['<rootDir>test/setup.js'],
+  snapshotSerializers: ['enzyme-to-json/serializer'],
+  testPathIgnorePatterns: ['/node_modules/', '/integration-test/'],
+}
